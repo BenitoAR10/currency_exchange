89d0f36f4b140ea4ab2a4b38408b93be2377fefb
unknown <https://github.com/BenitoAR10?tab=repositories>
2023-02-12 21:07:41 -0400

    bl creado

--- src/main/java/arquitectura/software/demo_c_e/bl/CurrencyBl.java
+++ src/main/java/arquitectura/software/demo_c_e/bl/CurrencyBl.java
@@ -1,2 +1,35 @@
-package arquitectura.software.demo_c_e.bl;public class CurrencyBl {
+package arquitectura.software.demo_c_e.bl;
+
+import arquitectura.software.demo_c_e.dto.ExchangeDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class CurrencyBl {
+
+    public ExchangeDto exchange(String from, String to, BigDecimal amount){
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.apilayer.com/exchangerates_data/convert?from=" + from + "&to=" + to + "&amount=" + amount)
+                .addHeader("apikey", "YEraXsSahmYnVTklREwJ6uvCDwm3VpVS")
+                .build();
+
+        try{
+            Response response = client.newCall(request).execute();
+            String result = response.body().string();
+            System.out.println(result);
+
+            ObjectMapper mapper = new ObjectMapper();
+            ExchangeDto exchangeDto = mapper.readValue(result, ExchangeDto.class);
+            return exchangeDto;
+        } catch (Exception e){
+            throw new RuntimeException("Error al llamar la API");
+        }
+    }
 }
