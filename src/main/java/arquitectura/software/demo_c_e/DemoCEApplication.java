778a107bfa3455e9a30c10ff106dfda36875a250
unknown <https://github.com/BenitoAR10?tab=repositories>
2023-02-12 20:53:54 -0400

    primer commit. Subiendo proyecto a mi repositorio

---
+++ src/main/java/arquitectura/software/demo_c_e/DemoCEApplication.java
@@ -0,0 +1,13 @@
+package arquitectura.software.demo_c_e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arquitectura/software/demo_c_e/DemoCEApplicationTests.java
@@ -0,0 +1,13 @@
+package arquitectura.software.demo_c_e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
