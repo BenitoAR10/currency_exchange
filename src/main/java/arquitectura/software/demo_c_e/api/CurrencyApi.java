92745f5a8aefbfcb8d1a2713121668b85e2311d7
unknown <https://github.com/BenitoAR10?tab=repositories>
2023-02-12 22:09:34 -0400

    haciendo la validacion

--- src/main/java/arquitectura/software/demo_c_e/api/CurrencyApi.java
+++ src/main/java/arquitectura/software/demo_c_e/api/CurrencyApi.java
@@ -4,6 +4,8 @@ package arquitectura.software.demo_c_e.api;
 import arquitectura.software.demo_c_e.bl.CurrencyBl;
 import arquitectura.software.demo_c_e.dto.ExchangeDto;
 import arquitectura.software.demo_c_e.dto.ResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.Currency;
 
 @RestController
 @RequestMapping("/api/v1")
 public class CurrencyApi {
 
+    private static Logger logger = LoggerFactory.getLogger(Currency.class);
     private CurrencyBl currencyBl;
 
 
@@ -25,8 +29,10 @@ public class CurrencyApi {
 
     @GetMapping("/currency")
     public ResponseEntity<ResponseDto<ExchangeDto>> getExchange(@RequestParam String from, @RequestParam String to, @RequestParam BigDecimal amount){
+        logger.info("Empezando la logica de negocio");
         ExchangeDto exchangeDto = currencyBl.exchange(from, to, amount);
         ResponseDto<ExchangeDto> responseDto = new ResponseDto<ExchangeDto>(exchangeDto, true, null);
+        logger.info("Enviando Respuesta");
         return ResponseEntity.ok(responseDto);
     }
 }
--- src/main/java/arquitectura/software/demo_c_e/bl/CurrencyBl.java
+++ src/main/java/arquitectura/software/demo_c_e/bl/CurrencyBl.java
@@ -1,16 +1,19 @@
 package arquitectura.software.demo_c_e.bl;
 
 import arquitectura.software.demo_c_e.dto.ExchangeDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
 public class CurrencyBl {
+
+    Logger logger = LoggerFactory.getLogger(CurrencyBl.class);
 
     public ExchangeDto exchange(String from, String to, BigDecimal amount){
         OkHttpClient client = new OkHttpClient();
@@ -21,7 +24,9 @@ public class CurrencyBl {
                 .build();
 
         try{
+            logger.info("Invocando a la API");
             Response response = client.newCall(request).execute();
+            logger.info("Procesando la respuesta");
             String result = response.body().string();
             System.out.println("hola");
             System.out.println(result);
@@ -32,6 +37,7 @@ public class CurrencyBl {
             return null;
 
         } catch (Exception e){
+            logger.error("Error al llamar la API", e);
             throw new RuntimeException("Error al llamar la API");
         }
     }
